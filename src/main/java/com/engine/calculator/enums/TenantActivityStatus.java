901a9d8ba7c1432d2fd63b69894c80c4d149782f
Raphael Willian <raphaelwillian.cardososilva@gmail.com>
2026-09-17 15:31:13 -0300

    feat: Adicionada Model Tenant
    Neste commit foi adicionada a Model Tenant com seus atributos iniciais. Foi necessário a criação do Enum de Status de atividade para compor os atributos.

---
+++ src/main/java/com/engine/calculator/enums/TenantActivityStatus.java
@@ -0,0 +1,8 @@
+package com.engine.calculator.enums;
+
+public enum TenantActivityStatus {
+
+    ON,
+    OFF
+
+}
---
+++ src/main/java/com/engine/calculator/models/Tenant.java
@@ -0,0 +1,31 @@
+package com.engine.calculator.models;
+
+
+import com.engine.calculator.enums.TenantActivityStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tenant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "tenant_id", unique = true, nullable = false)
+    private UUID idTenant;
+
+    @Column(name = "name_tenant", length = 255, nullable = false)
+    private String nameTenant;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "status_activity")
+    private TenantActivityStatus statusActivity;
+
+}
