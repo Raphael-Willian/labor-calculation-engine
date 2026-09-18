487921f90b9ff5f772b89598ff31a039c434d7f7
Raphael Willian <raphaelwillian.cardososilva@gmail.com>
2026-09-18 17:28:10 -0300

    feat: Estabelecimento de Relacionamento com RuleParameter
    O relacionamento foi estabelecido, porém algumas coisas foram consertadas, como: a nomação da coluna id; a adição de @Generated para hibernate.

--- src/main/java/com/engine/calculator/models/RuleVersion.java
+++ src/main/java/com/engine/calculator/models/RuleVersion.java
@@ -4,9 +4,12 @@ import com.engine.calculator.enums.StatusVersionRule;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Generated;
+import org.hibernate.generator.EventType;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -17,6 +20,7 @@ public class RuleVersion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "version_rule_id", nullable = false, updatable = false, unique = true)
     private UUID idVersionRule;
 
     @ManyToOne(optional = false)
@@ -24,6 +28,7 @@ public class RuleVersion {
     private CalculationRule rule;
 
     @Column(name = "valid_from", nullable = false, columnDefinition = "DATE DEFAULT CURRENT_DATE", updatable = true)
+    @Generated(event = EventType.INSERT)
     private LocalDate validFrom; //Válido a partir de...
 
     @Column(name = "valid_until", updatable = true)
@@ -32,6 +37,10 @@ public class RuleVersion {
     @ManyToOne(optional = false)
     @JoinColumn(name = "create_by", nullable = false, updatable = false, referencedColumnName = "user_name")
     private User createdBy;
+
+    @OneToMany(mappedBy = "idParameterRule", cascade = CascadeType.REMOVE)
+    @Column(name = "parameters", nullable = false, updatable = true)
+    private List<RuleParameter> parameters;
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createAt = LocalDateTime.now();
